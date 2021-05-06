9f0e7603e48931e6151ba44bd48af890ed2a2a39
Ben Latham <benlatham123@hotmail.co.uk>
2021-05-06 15:17:39 +0100

    remove version

--- package1/setup.py
+++ package1/setup.py
@@ -2,6 +2,5 @@ from distutils.core import setup
 
 setup(
     name="package1",
-    version="1.0",
     py_modules=["alertme"],
 )
--- package2/setup.py
+++ package2/setup.py
@@ -2,6 +2,5 @@ from distutils.core import setup
 
 setup(
     name="package2",
-    version="1.0",
     py_modules=["alertme"],
 )
